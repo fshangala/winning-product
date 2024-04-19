7b312e9fa79906f8685a12601a69e6a0c9c1b76f
fshangala <fshangala@gmail.com>
2024-04-19 06:03:25 +0200

    static

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 document.addEventListener("alpine:init",function(){
+  Alpine.store("user",()=>({
+    supabaseUrl : 'http://copiwin.com:8000',
+    supabaseKey : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzEzMzA0ODAwLAogICJleHAiOiAxODcxMDcxMjAwCn0.wzm6GJDkA5uIKsaUcZPHG2mm-lCDIHCaFX1GiWlXDtQ",
+    supabase : createClient(this.supabaseUrl, this.supabaseKey),
+  }));
+
   Alpine.data("facebookFilter",()=>({
+    search:"",
     keywordIn:"ad-text",
     country:["usa","united-kingdom"],
     website:"shopify",
@@ -17,6 +24,12 @@ document.addEventListener("alpine:init",function(){
     sortBy:"consistency",
     sortDirection:"ascending",
     scaling:"no-downscaling",
+    mediaType:"videos",
+    pageType:"product-pages",
+    niche: "beauty",
+    adCreationDate:"",
+    lastSeenDate:"",
+    productCreationDate:"",
   }))
 
   Alpine.data("navigationBar",()=>({
